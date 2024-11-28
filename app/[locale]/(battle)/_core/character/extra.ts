eac200a6dde7a371eb4700fd88d8091a26792093
kychongdev <kychongdev@gmail.com>
2024-11-28 15:33:36 +0800

    fix: 10134 missing extra

--- app/[locale]/(battle)/_core/character/extra.ts
+++ app/[locale]/(battle)/_core/character/extra.ts
@@ -44,13 +44,13 @@ export function applyExtra(gameState: GameState, position: number) {
         }
       });
       break;
-    case '802':
+    case '10134':
       // 使自身獲得『攻擊時，觸發「以自身攻擊力0/0/10/12.5/15使自身以外我方全體攻擊力增加(1回合)」』(5回合)
       if (gameState.characters[position].bond > 2) {
         gameState.characters[position].buff = [
           ...gameState.characters[position].buff,
           {
-            id: '802-ult-1',
+            id: '10134-ult-1',
             name: '攻擊時，觸發『以自身攻擊力使自身以外我方全體攻擊力增加』(1回合)',
             type: 6,
             condition: Condition.ATTACK,
