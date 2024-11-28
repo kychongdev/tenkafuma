1820ae5c9904a01eb952174364da12286bc77076
kychongdev <kychongdev@gmail.com>
2024-11-28 14:52:59 +0800

    fix: change header to button

--- components/default/HeaderButton.tsx
+++ components/default/HeaderButton.tsx
@@ -1,18 +1,20 @@
 'use client';
 import { useRouter } from '@/app/i18n/routing';
 import { useTranslations } from 'next-intl';
+import { Button } from '../ui/button';
 
 export const HeaderButton = () => {
   const t = useTranslations('AppSidebar');
   const router = useRouter();
   return (
-    <div
+    <Button
+      variant={'ghost'}
       className="font-extrabold"
       onClick={() => {
         router.push('/');
       }}
     >
       {t('header-title')}
-    </div>
+    </Button>
   );
 };
