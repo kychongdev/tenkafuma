227f949bb6cec513753e37c77bf565081f64098b
kychongdev <kychongdev@gmail.com>
2024-11-28 14:17:35 +0800

    fix: attempt

--- app/[locale]/(battle)/simulate/result/[index]/page.tsx
+++ app/[locale]/(battle)/simulate/result/[index]/page.tsx
@@ -6,10 +6,11 @@ import { isString } from 'lodash';
 import { useSimulateTeamState } from '@/core/SimulateTeamState';
 import { SimulateResult } from './SimulateResult';
 export default function SimulateSelect() {
-  const { index } = useParams();
-  if (!index || !isString(index)) {
-    return <div>Wrong Parameter</div>;
-  }
+  const params = useParams();
+  const index = params.index as string;
+  // if (!index || !isString(index)) {
+  //   return <div>Wrong Parameter</div>;
+  // }
   // const hasHydrated = useSimulateTeamState((state) => state._hasHydrated);
   //
   // if (!hasHydrated) {
