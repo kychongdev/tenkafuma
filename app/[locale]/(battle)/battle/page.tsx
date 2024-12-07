8e935976adad9ec96144307a4ea9fdb7d60b40b1
kychongdev <kychongdev@gmail.com>
2024-12-06 21:40:02 -0600

    fix formatter

--- app/[locale]/(battle)/battle/page.tsx
+++ app/[locale]/(battle)/battle/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { ClientOnly } from '@/components/ClientOnly';
-import Battle from './Battle';
+import { ClientOnly } from "@/components/ClientOnly";
+import Battle from "./Battle";
 
 export default function BattlePage() {
-        return (
-                <ClientOnly>
-                        <Battle />
-                </ClientOnly>
-        );
+  return (
+    <ClientOnly>
+      <Battle />
+    </ClientOnly>
+  );
 }
